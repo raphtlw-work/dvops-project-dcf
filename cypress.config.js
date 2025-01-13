2f6e1989e0076eddde3bd91b5bf665982fdf3751
Raphael Tang <raphpb1912@gmail.com>
2025-01-14 01:14:16 +0800

    fix: pls work

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ import "dotenv/config"
 import { defineConfig } from "cypress"
 import { GenerateCtrfReport } from "cypress-ctrf-json-reporter"
 import { sql } from "drizzle-orm"
+import { db } from "./cypress/build/util/db"
 
 export default defineConfig({
   e2e: {
@@ -11,17 +12,17 @@ export default defineConfig({
       on("task", {
         async clearDB() {
           // prettier-ignore
-          const query = sql<string>`
+          const query = sql `
             SELECT table_name
             FROM information_schema.tables
             WHERE table_schema = 'public'
               AND table_type = 'BASE TABLE`
 
-          const tables = await this.db.execute(query) // retrieve tables
+          const tables = await db.execute(query) // retrieve tables
 
           for (let table of tables) {
             const query = sql.raw(`TRUNCATE TABLE ${table.table_name} CASCADE;`)
-            await this.db.execute(query) // Truncate (clear all the data) the table
+            await db.execute(query) // Truncate (clear all the data) the table
           }
         },
       })
