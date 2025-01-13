985d56012d2949a92df045c31ec95fd024460500
Raphael Tang <raphpb1912@gmail.com>
2025-01-14 03:40:54 +0800

    fix: don't clean files before build

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   entry: ["index.ts"],
-  clean: true,
+  clean: false,
   format: "esm",
   target: "esnext",
   sourcemap: true,
