da594da0be8f549e05d8dc96124e722d6f289d5f
Raphael Tang <raphpb1912@gmail.com>
2025-01-14 01:11:03 +0800

    fix: pls work

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ import "dotenv/config"
 
 import { defineConfig } from "cypress"
 import { GenerateCtrfReport } from "cypress-ctrf-json-reporter"
-import { usersTable } from "./cypress/build/schema/db"
-import { db } from "./cypress/build/util/db"
+import { sql } from "drizzle-orm"
 
 export default defineConfig({
   e2e: {
@@ -11,8 +10,19 @@ export default defineConfig({
       // implement node event listeners here
       on("task", {
         async clearDB() {
-          const result = await db.delete(usersTable)
-          return result
+          // prettier-ignore
+          const query = sql<string>`
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+              AND table_type = 'BASE TABLE`
+
+          const tables = await this.db.execute(query) // retrieve tables
+
+          for (let table of tables) {
+            const query = sql.raw(`TRUNCATE TABLE ${table.table_name} CASCADE;`)
+            await this.db.execute(query) // Truncate (clear all the data) the table
+          }
         },
       })
       new GenerateCtrfReport({ on })
