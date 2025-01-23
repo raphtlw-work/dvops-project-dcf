2375035d22db185dd870010b043a9d90767f7cd6
Raphael Tang <raphpb1912@gmail.com>
2025-01-23 16:00:20 +0800

    chore: used absolute import paths

--- util/db.ts
+++ util/db.ts
@@ -1,7 +1,7 @@
 import * as schema from "@/schema/db"
 import { drizzle } from "drizzle-orm/node-postgres"
 
-export const getConnectionString = () => process.env.POSTGRES_CONNECTION_URL
+export const getConnectionString = () => process.env.POSTGRES_CONNECTION_URL!
 
 export const db = drizzle(getConnectionString(), {
   schema,
